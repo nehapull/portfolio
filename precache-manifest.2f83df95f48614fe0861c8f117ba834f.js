2947573c76957778da78ca26c7b6504b04ea77d5
nehapull <neha.pullabhotla@gmail.com>
2020-04-22 23:06:20 -0700

    Updates

---
+++ precache-manifest.2f83df95f48614fe0861c8f117ba834f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19279a944f039514df942a5ffb93e09",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3f3da7b1cdb6b625abcf",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7f141faa93254d62ba5a",
+    "url": "/portfolio/static/js/2.d90ae76b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.d90ae76b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f3da7b1cdb6b625abcf",
+    "url": "/portfolio/static/js/main.19348c83.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
